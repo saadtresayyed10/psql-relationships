7af32a9f0a05a063119f7dafb1449b375ec1ad0f
unknown <saadsyed950@gmail.com>
2025-01-23 15:52:33 +0530

    1-to-M working just fine

--- users-goals-one-to-many.sql
+++ users-goals-one-to-many.sql
@@ -22,13 +22,41 @@ INSERT INTO users (username, email) VALUES
 INSERT INTO goals (goalname, user_id) VALUES 
 ('Goal-1', 1),
 ('Goal-2', 1),
-('Goal-3', 1);
+('Goal-3', 1),
 ('Goal-4', 2),
 ('Goal-5', 2),
-('Goal-6', 3),
+('Goal-6', 3);
 
 SELECT * FROM users;
-SELECT * FROM goals;
+=>
+    userid | username |      email
+    --------+----------+-----------------
+        1 | Saad     | saad@gmail.com
+        2 | Ayman    | ayman@gmail.com
+        3 | Amaan    | amaan@gmail.com
+    (3 rows)
 
-SELECT username, email FROM users
+SELECT * FROM goals;
+=>
+    goalid | goalname | user_id
+    --------+----------+---------
+        1 | Goal-1   |       1
+        2 | Goal-2   |       1
+        3 | Goal-3   |       1
+        4 | Goal-4   |       2
+        5 | Goal-5   |       2
+        6 | Goal-6   |       3
+    (6 rows)
+
+SELECT username, goalname, email  FROM users
 JOIN goals ON goals.user_id = users.userid;
+=>
+    username | goalname |      email
+    ----------+----------+-----------------
+    Saad     | Goal-1   | saad@gmail.com
+    Saad     | Goal-2   | saad@gmail.com
+    Saad     | Goal-3   | saad@gmail.com
+    Ayman    | Goal-4   | ayman@gmail.com
+    Ayman    | Goal-5   | ayman@gmail.com
+    Amaan    | Goal-6   | amaan@gmail.com
+    (6 rows)
