5d3bf4673f9972996bbd8dfbedf4ebd12c5a07dd
unknown <saadsyed950@gmail.com>
2025-01-23 15:44:00 +0530

    Working just fine

--- users-goals.sql
+++ users-goals.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
 CREATE TABLE goals (
     goalid SERIAL PRIMARY KEY,
     goalname VARCHAR(30),
-    user_id INT REFERENCES users (id),
+    user_id INT REFERENCES users (userid),
     UNIQUE(user_id)
 );
 
@@ -23,8 +23,35 @@ INSERT INTO users (username, email) VALUES
 INSERT INTO goals (goalname, user_id) VALUES 
 ('Goal-1', 1),
 ('Goal-2', 2),
-('Goal-3', 3),
-('Goal-4', 4);
+('Goal-3', 3);
+
+
+SELECT * FROM users;
+=>
+    userid | username |      email
+    --------+----------+-----------------
+        1 | Saad     | saad@gmail.com
+        2 | Ayman    | ayman@gmail.com
+        3 | Amaan    | amaan@gmail.com
+    (3 rows)
+
+
+SELECT * FROM goals;
+=>
+    goalid | goalname | user_id
+    --------+----------+---------
+        5 | Goal-1   |       1
+        6 | Goal-2   |       2
+        7 | Goal-3   |       3
+    (3 rows)
+
 
 SELECT username, email FROM users
 JOIN goals ON goals.user_id = users.userid;
+=>
+    username |      email
+    ----------+-----------------
+    Saad     | saad@gmail.com
+    Ayman    | ayman@gmail.com
+    Amaan    | amaan@gmail.com
+    (3 rows)
