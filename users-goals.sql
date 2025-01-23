50faa534c0efe1404781373098a6ecd42687b1de
unknown <saadsyed950@gmail.com>
2025-01-23 15:38:15 +0530

    One-to-one relationship commands are done

--- users-goals.sql
+++ users-goals.sql
@@ -19,3 +19,12 @@ INSERT INTO users (username, email) VALUES
 ('Saad','saad@gmail.com'), 
 ('Ayman','ayman@gmail.com'), 
 ('Amaan','amaan@gmail.com');
+
+INSERT INTO goals (goalname, user_id) VALUES 
+('Goal-1', 1),
+('Goal-2', 2),
+('Goal-3', 3),
+('Goal-4', 4);
+
+SELECT username, email FROM users
+JOIN goals ON goals.user_id = users.userid;
